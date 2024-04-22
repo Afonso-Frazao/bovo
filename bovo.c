9f6eeebe97b819a180020cea1609a4ef19ca2c12
Afonso <afonso.frazao@tecnico.ulisboa.pt>
2024-04-22 22:49:29 +0100

    Estou a fazer com que a última jogada fique destacada

--- bovo.c
+++ bovo.c
@@ -1,26 +1,39 @@
 #include "head.h"
+#include <stdio.h>
 
-/*int printingtime(int **board) {
+typedef struct {
+  int l;
+  int c;
+} coords;
+
+int printingtime(int **board, int lastl, int lastc) {
   int i, j;
 
+  printf("\n");
   for (i = 0; i < 23; i++) {
     for (j = 0; j < 23; j++) {
       if (board[i][j] != 0) {
         if ((j % 2) == 0) {
-          if (board[i][j] == 'O') {
-            printf("\033[38;5;39mO\033[0m|\033[0m"); // Color code 39 for orange
-          } else if (board[i][j] == 'X') {
-            printf("\033[38;5;208mX\033[0m|\033[0m"); // Color code 208 for cyan
+          if (board[i][j] == 'X') {
+            printf("\033[38;5;208mX\033[0m|"); // Color code 208 for orange
+          } else if (board[i][j] == 'O') {
+            printf("\033[38;5;39mO\033[0m|"); // Color code 39 for cyan
+          } else if (board[i][j] == -'X') {
+            printf("\033[38;5;34mX\033[0m|");
+          } else if (board[i][j] == -'O') {
+            printf("\033[38;5;34mO\033[0m|");
           } else {
             printf("%c|", board[i][j]);
           }
         } else {
-          if (board[i][j] == 'O') {
-            printf("\033[38;5;39mO\033[38;5;240m|\033[0m"); // Color code 39 for
-                                                            // cyan
-          } else if (board[i][j] == 'X') {
-            printf("\033[38;5;208mX\033[38;5;240m|\033[0m"); // Color code 208
-                                                             // for orange
+          if (board[i][j] == 'X') {
+            printf("\033[38;5;208mX\033[38;5;240m|\033[0m");
+          } else if (board[i][j] == 'O') {
+            printf("\033[38;5;39mO\033[38;5;240m|\033[0m");
+          } else if (board[i][j] == -'X') {
+            printf("\033[38;5;34mX\033[38;5;240m|\033[0m");
+          } else if (board[i][j] == -'O') {
+            printf("\033[38;5;34mO\033[38;5;240m|\033[0m");
           } else {
             printf("%c\033[38;5;240m|\033[0m", board[i][j]);
           }
@@ -38,13 +51,15 @@
   printf("\n");
 
   return 0;
-}*/
+}
 
 int main() {
   int **board;
   int i;
   char l, c;
   int gameover = 0;
+  coords *moves;
+  int playsN;
 
   board = (int **)malloc(23 * sizeof(int *));
   for (i = 0; i < 23; i++) {
@@ -56,10 +71,20 @@ int main() {
     board[0][i] = i + 'a' - 1;
   }
 
+  moves = (coords *)calloc(484, sizeof(int));
+
+  playsN = 0;
   while (gameover == 0) {
     for (i = 0; i < 2; i++) {
-      if (printingtime(board) != 0) {
-        return 1;
+      if (playsN != 0) {
+        if (printingtime(board, moves[playsN - 1].l, moves[playsN - 1].c) !=
+            0) {
+          return 1;
+        }
+      } else {
+        if (printingtime(board, 0, 0) != 0) {
+          return 1;
+        }
       }
       // fflush(stdout);
 
@@ -67,35 +92,79 @@ int main() {
       l = l - 'a' + 1;
       c = c - 'a' + 1;
 
-      if (i == 0) {
-        if (board[l][c] == 0) {
-          board[l][c] = 'X';
-        } else {
-          printf("Invalid move\n");
-          i--;
-        }
-      } else {
-        if (board[l][c] == 0) {
-          board[l][c] = 'O';
-        } else {
-          printf("Invalid move\n");
-          i--;
-        }
+      if ((l < 1 || l > 22 || c < 1 || c > 22) && (l != 'z' - 'a' + 1) &&
+          (l != 'y' - 'a' + 1)) {
+        printf("\nInvalid move!\n");
+        i--;
+        continue;
       }
 
-      gameover = verifygameover(board);
-      if (gameover == 1) {
+      if (l == 'z' - 'a' + 1) {
+        if (playsN == 0) {
+          printf("\nAlready at empty board!\n");
+          i--;
+          continue;
+        }
+        playsN--;
+        board[moves[playsN].l][moves[playsN].c] = 0;
+        if (printingtime(board, moves[playsN - 1].l, moves[playsN - 1].c) !=
+            0) {
+          return 1;
+        }
+      } else if (l == 'y' - 'a' + 1) {
+        if (moves[playsN].l == 0) {
+          printf("\nAlready at last play!\n");
+          i--;
+          continue;
+        }
         if (i == 0) {
-          if (printingtime(board) != 0) {
-            return 1;
-          }
-          printf("\n\033[38;5;208mX\033[0m wins!\n\n");
-          break;
+          board[moves[playsN].l][moves[playsN].c] = 'X';
         } else {
-          if (printingtime(board) != 0) {
-            return 1;
+          board[moves[playsN].l][moves[playsN].c] = 'O';
+        }
+        if (printingtime(board, moves[playsN].l, moves[playsN].c) != 0) {
+          return 1;
+        }
+        playsN++;
+      } else {
+        if (i == 0) {
+          if (board[l][c] == 0) {
+            board[l][c] = 'X';
+            moves[playsN].l = l;
+            moves[playsN].c = c;
+            playsN++;
+          } else {
+            printf("\nInvalid move\n");
+            i--;
+            continue;
           }
-          printf("\n\033[38;5;39mO\033[0m wins!\n\n");
+        } else {
+          if (board[l][c] == 0) {
+            board[l][c] = 'O';
+            moves[playsN].l = l;
+            moves[playsN].c = c;
+            playsN++;
+          } else {
+            printf("\nInvalid move\n");
+            i--;
+            continue;
+          }
+        }
+
+        gameover = verifygameover(board);
+        if (gameover == 1) {
+          if (i == 0) {
+            if (printingtime(board, l, c) != 0) {
+              return 1;
+            }
+            printf("\n\033[38;5;208mX\033[0m wins!\n\n");
+            break;
+          } else {
+            if (printingtime(board, l, c) != 0) {
+              return 1;
+            }
+            printf("\n\033[38;5;39mO\033[0m wins!\n\n");
+          }
         }
       }
     }
--- eval.c
+++ eval.c
@@ -1,47 +1,6 @@
 #include "head.h"
-#include <unistd.h>
 
 int gamealreadyover(int **board) { return 0; }
-
-int printingtime(int **board) {
-  int i, j;
-
-  for (i = 0; i < 23; i++) {
-    for (j = 0; j < 23; j++) {
-      if (board[i][j] != 0) {
-        if ((j % 2) == 0) {
-          if (board[i][j] == 'O') {
-            printf("\033[38;5;39mO\033[0m|\033[0m"); // Color code 39 for orange
-          } else if (board[i][j] == 'X') {
-            printf("\033[38;5;208mX\033[0m|\033[0m"); // Color code 208 for cyan
-          } else {
-            printf("%c|", board[i][j]);
-          }
-        } else {
-          if (board[i][j] == 'O') {
-            printf("\033[38;5;39mO\033[38;5;240m|\033[0m"); // Color code 39 for
-                                                            // cyan
-          } else if (board[i][j] == 'X') {
-            printf("\033[38;5;208mX\033[38;5;240m|\033[0m"); // Color code 208
-                                                             // for orange
-          } else {
-            printf("%c\033[38;5;240m|\033[0m", board[i][j]);
-          }
-        }
-      } else {
-        if ((j % 2) == 0) {
-          printf(" |");
-        } else {
-          printf(" \033[38;5;240m|\033[0m");
-        }
-      }
-    }
-    printf("\n");
-  }
-  printf("\n");
-
-  return 0;
-}
 
 int evaluateposition(int **board) {
   int eval; // The 'X' will get a positive eval while the 'O' will get a
@@ -77,14 +36,9 @@ int evaluateposition(int **board) {
 int verifygameover(int **board) {
   int i, j, diag;
   int streak, symbol;
-  int lucio;
 
   for (streak = 0, symbol = 'X', i = 1; i < 23; i++) { // verify lines
     for (j = 1; j < 23; j++) {
-      board[i][j] = 'O';
-      printingtime(board);
-      board[i][j] = 0;
-      usleep(50000);
       if (board[i][j] == symbol) {
         streak++;
       } else if (board[i][j] != 0) {
@@ -94,6 +48,12 @@ int verifygameover(int **board) {
         streak = 0;
       }
       if (streak == 5) {
+        board[i][j] = -symbol;
+        board[i][j - 1] = -symbol;
+        board[i][j - 2] = -symbol;
+        board[i][j - 3] = -symbol;
+        board[i][j - 4] = -symbol;
+
         return 1;
       }
     }
@@ -101,10 +61,6 @@ int verifygameover(int **board) {
 
   for (streak = 0, symbol = 'X', i = 1; i < 23; i++) { // verify rows
     for (j = 1; j < 23; j++) {
-      board[j][i] = 'O';
-      printingtime(board);
-      board[j][i] = 0;
-      usleep(50000);
       if (board[j][i] == symbol) {
         streak++;
       } else if (board[j][i] != 0) {
@@ -114,6 +70,12 @@ int verifygameover(int **board) {
         streak = 0;
       }
       if (streak == 5) {
+        board[j][i] = -symbol;
+        board[j - 1][i] = -symbol;
+        board[j - 2][i] = -symbol;
+        board[j - 3][i] = -symbol;
+        board[j - 4][i] = -symbol;
+
         return 1;
       }
     }
@@ -122,10 +84,6 @@ int verifygameover(int **board) {
   for (streak = 0, symbol = 'X', diag = 22; diag >= 1;
        diag--) { // verify diagonal 1
     for (i = 1, j = diag; j < 23; i++, j++) {
-      board[i][j] = 'O';
-      printingtime(board);
-      board[i][j] = 0;
-      usleep(50000);
       if (board[i][j] == symbol) {
         streak++;
       } else if (board[i][j] != 0) {
@@ -135,6 +93,12 @@ int verifygameover(int **board) {
         streak = 0;
       }
       if (streak == 5) {
+        board[i][j] = -symbol;
+        board[i - 1][j - 1] = -symbol;
+        board[i - 2][j - 2] = -symbol;
+        board[i - 3][j - 3] = -symbol;
+        board[i - 4][j - 4] = -symbol;
+
         return 1;
       }
     }
@@ -143,10 +107,6 @@ int verifygameover(int **board) {
   for (streak = 0, symbol = 'X', diag = 2; diag < 23;
        diag++) { // verify diagonal 1
     for (i = diag, j = 1; i < 23; i++, j++) {
-      board[i][j] = 'O';
-      printingtime(board);
-      board[i][j] = 0;
-      usleep(50000);
       if (board[i][j] == symbol) {
         streak++;
       } else if (board[i][j] != 0) {
@@ -156,6 +116,12 @@ int verifygameover(int **board) {
         streak = 0;
       }
       if (streak == 5) {
+        board[i][j] = -symbol;
+        board[i - 1][j - 1] = -symbol;
+        board[i - 2][j - 2] = -symbol;
+        board[i - 3][j - 3] = -symbol;
+        board[i - 4][j - 4] = -symbol;
+
         return 1;
       }
     }
@@ -164,10 +130,6 @@ int verifygameover(int **board) {
   for (streak = 0, symbol = 'X', diag = 1; diag < 23;
        diag++) { // verify diagonal 2
     for (i = diag, j = 1; i >= 1; i--, j++) {
-      board[i][j] = 'O';
-      printingtime(board);
-      board[i][j] = 0;
-      usleep(50000);
       if (board[i][j] == symbol) {
         streak++;
       } else if (board[i][j] != 0) {
@@ -177,6 +139,12 @@ int verifygameover(int **board) {
         streak = 0;
       }
       if (streak == 5) {
+        board[i][j] = -symbol;
+        board[i + 1][j - 1] = -symbol;
+        board[i + 2][j - 2] = -symbol;
+        board[i + 3][j - 3] = -symbol;
+        board[i + 4][j - 4] = -symbol;
+
         return 1;
       }
     }
@@ -185,10 +153,6 @@ int verifygameover(int **board) {
   for (streak = 0, symbol = 'X', diag = 2; diag < 23;
        diag++) { // verify diagonal 2
     for (i = 22, j = diag; j < 23; i--, j++) {
-      board[i][j] = 'O';
-      printingtime(board);
-      board[i][j] = 0;
-      usleep(50000);
       if (board[i][j] == symbol) {
         streak++;
       } else if (board[i][j] != 0) {
@@ -198,6 +162,12 @@ int verifygameover(int **board) {
         streak = 0;
       }
       if (streak == 5) {
+        board[i][j] = -symbol;
+        board[i + 1][j - 1] = -symbol;
+        board[i + 2][j - 2] = -symbol;
+        board[i + 3][j - 3] = -symbol;
+        board[i + 4][j - 4] = -symbol;
+
         return 1;
       }
     }
